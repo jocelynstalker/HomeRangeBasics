a82a80a6979118937446d09cd42a4f8cb13f6eaf
jocelynstalker <72157783+jocelynstalker@users.noreply.github.com>
2020-10-08 17:20:15 -0500

    Delete HomeRangeBasics.R

--- HomeRangeBasics.R
+++
@@ -1,3 +0,0 @@
-packages<-c("adehabitatHR","data.table","ggfortify","grid","move","moveVis","OpenStreetMap","pbapply","plotly","rgdal","sp","tidyverse","viridis")
-sapply(packages, require, character.only=T)
-read.csv()
